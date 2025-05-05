f7b740ca202aa82e1b127979847cf660f9ac9d41
mahmoudsafwat20 <ms1383909@gmail.com>
2025-05-05 21:48:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number of the small ones : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the number of the large ones : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = (x * 25) + (y * 35);
+            Console.WriteLine($"the totle price after the tax : {((z * 0.06) + z)}");
+        }
+    }
+}
